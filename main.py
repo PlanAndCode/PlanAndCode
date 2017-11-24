517e82e3bf118f2c529c9f2051a0922934f821c4
aydincalikoglu <aykocayko@gmail.com>
2017-11-24 23:20:56 +0300

    Trello API-key and Token Object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from trelloAPI import trelloAPI
 from githubAPI import githubapi
 
+from trello import TrelloClient
 
 
 
@@ -14,7 +15,7 @@ github = githubapi.github()
 
 
 
-trello = trelloAPI.trello("asdasd","asdasd")
+trello = trelloAPI.trello(apiKey="6a4fe89f7b7bd584332a3cecf685d25b",TOKEN="0c54fb091e9723f7696bbb2a9cdd405741d7b67ad2deeb9d653afe009c2a61a9")
 print(trello.createPano())
 print("test2")
 
--- trelloAPI/trelloAPI.py
+++ trelloAPI/trelloAPI.py
@@ -1,7 +1,20 @@
+
+from trello import TrelloClient
+
 class trello:
     def __init__(self,apiKey,TOKEN):
-        self.tokenKey=TOKEN
-        self.apiKey=apiKey
+        self.client = TrelloClient(
+            api_key=apiKey,
+            api_secret='your-secret',
+            token=TOKEN,
+            token_secret='your-oauth-token-secret'
+        )
+
+    def listPanos(self):
+        all_boards = self.client.list_boards()
+        last_board = all_boards[-1]
+        print(last_board.name)
+
 
 
 
