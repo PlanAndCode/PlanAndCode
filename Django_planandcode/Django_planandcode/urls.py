cb0ff38a6330defb9f2a969714a4c916767af72b
HaticeKubra <hkteloglu@gtu.edu.tr>
2017-11-25 23:47:18 +0300

    update urls.py with home_view

--- Django_planandcode/Django_planandcode/urls.py
+++ Django_planandcode/Django_planandcode/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from post.views import home_view
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
+    url(r'^$',home_view)
 ]
+
