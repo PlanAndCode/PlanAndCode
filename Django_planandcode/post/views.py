d6dab59c459c10a265c62512e925cfc3e79949a3
Gulzada IISAEVA <iisaevagulzada@gmail.com>
2017-12-01 14:29:42 +0300

    Update views.py

--- Django_planandcode/post/views.py
+++ Django_planandcode/post/views.py
@@ -1,23 +1,25 @@
+
 from django.shortcuts import render
 from .githubAPI import *
 from django.template.loader import get_template
 from django.http import HttpResponse
 import datetime
 
+deneme=None
 
 def index(request):
     global deneme
     if (request.method == "POST"):
         get_text = request.POST["textfield"]
         get_text2 = request.POST["textfield2"]
-
-        deneme = GitHubAPI(get_text, get_text2, "PlanAndCode")
+        get_text3 = request.POST["textfield3"]
+        deneme = GitHubAPI(get_text, get_text2, get_text3)
 
     return render(request, 'index.html', {})
 
 def page2(request):
-     t = None
-     if (request.method == "POST2"):
-        t = deneme.show_projects()
+    t=GitHubAPI("burakfurkanaksahin", "software2017", "PlanAndCode")
+    #show2=t.new_project( "deneme", "deneme", "deneme")
+    show=t.show_project()
+    return render(request,'page2.html',context={'show': show },)
 
-     return render(request, 'page2.html', {'show': t})
